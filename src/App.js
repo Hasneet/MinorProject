3911e34199b6c0e361d422f56936a386cbb9b9fb
Hasneet Singh <hasneetsingh@Hasneets-MacBook-Pro-2.local>
2023-12-13 21:45:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import { makeStyles } from "@material-ui/core";
-import Homepage from "./Pages/HomePage";
-import "./App.css";
-import { BrowserRouter, Route } from "react-router-dom";
-import CoinPage from "./Pages/CoinPage";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-
+import { makeStyles } from '@material-ui/core';
+import Homepage from './Pages/HomePage';
+import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import CoinPage from './Pages/CoinPage';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const useStyles = makeStyles(() => ({
   App: {
-    backgroundColor: "#14161a",
-    color: "white",
-    minHeight: "100vh",
+    backgroundColor: '#14161a',
+    color: 'white',
+    minHeight: '100vh',
   },
 }));
 
 function App() {
+  const isLoggedIn = localStorage.getItem('logged-in');
   const classes = useStyles();
 
   return (
     <BrowserRouter>
       <div className={classes.App}>
-        <Header />
-        <Route path="/" component={Homepage} exact />
-        <Route path="/coins/:id" component={CoinPage} exact />
+        {isLoggedIn && <Header />}
+        <Route path='/' component={Homepage} exact />
+        <Route path='/coins/:id' component={CoinPage} exact />
         <Footer />
       </div>
     </BrowserRouter>
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,14 +1,58 @@
-import React from "react";
-import Banner from "../components/Banner/Banner";
-import CoinsTable from "../components/CoinsTable";
-
+import React, { useEffect, useState } from 'react';
+import Banner from '../components/Banner/Banner';
+import CoinsTable from '../components/CoinsTable';
+import Login from '../components/Login';
+import { Button } from '@material-ui/core';
+import Intro from '../components/Intro';
 
 const Homepage = () => {
-  return (
+  const [_, setUser] = useState(false);
+  const [showCoinsTable, setShowCoinsTable] = useState(false);
+  const isLoggedIn = localStorage.getItem('logged-in');
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      setUser(true);
+    }
+  }, [isLoggedIn]);
+
+  const onLoginSuccess = () => {
+    setUser(true);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('logged-in');
+    setUser(false);
+  };
+
+  const handleShowCoinsTable = () => {
+    setShowCoinsTable(true);
+  };
+
+  return isLoggedIn ? (
     <>
       <Banner />
-      <CoinsTable />
+      {showCoinsTable ? (
+        <CoinsTable />
+      ) : (
+        <Intro handleShowCoinsTable={handleShowCoinsTable} />
+      )}
+      <Button
+        id='logout-btn'
+        onClick={handleLogout}
+        style={{
+          backgroundColor: 'red',
+          color: 'white',
+          position: 'absolute',
+          top: 10,
+          right: 10,
+        }}
+      >
+        Logout
+      </Button>
     </>
+  ) : (
+    <Login onLoginSuccess={onLoginSuccess} />
   );
 };
 
